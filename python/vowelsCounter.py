f321593b8cd43717806550a92f3894925d2e794d
Michelle <michellefrancis85@gmail.com>
2017-07-04 01:03:25 -0400

    vowelsCounter

--- python/vowelsCounter.py
+++ python/vowelsCounter.py
@@ -2,13 +2,13 @@ def vowelsCounter(word):
     vowels=['a','e','i','o','u']
 
     count=0;
-    for char in vowels:
-        if(char in word):
+    for char in word:
+        if(char in vowels):
             count = count+1
+            #print char
 
     return count
 
-print vowelsCounter('apples')
 print vowelsCounter('apples') # Expected: 2
 print vowelsCounter('aeiou') # Expected: 5
 print vowelsCounter('why') # Expected: 0
